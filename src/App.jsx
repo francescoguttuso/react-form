0e5aee6d3505481fb2b337bbc11765d98269a30d
francesco-guttuso <francescoguttuso@gmail.com>
2026-07-17 22:04:47 +0200

    added button delete item

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [items, setItems] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <>
       <div className="Lista">
         <ul>
-          {items.map((item) => (<li key={item}>{item}</li>))};
+          {items.map((item) => (
+            <li key={item}>
+              {item}{" "}
+              <button
+                onClick={() =>
+                  setItems(items.filter((_item) => _item !== item))
+                }
+              >
+                Elimina
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
 
-      <form onSubmit={e => {
-        e.preventDefault();
-        if (inputValue.trim() !== '') {
-          setItems(items => items.concat(inputValue));
-          setInputValue('');
-        }
-      }}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (inputValue.trim() !== "") {
+            setItems((items) => items.concat(inputValue));
+            setInputValue("");
+          }
+        }}
+      >
         <input
           type="text"
           placeholder="Inserisci nuovo elemento"
@@ -27,9 +40,9 @@ function App() {
           onChange={(e) => setInputValue(e.target.value)}
         />
         <button type="submit">Invia</button>
-      </form >
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
