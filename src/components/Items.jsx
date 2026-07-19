2e5500e00808528224258fcf4a7c9f5eac52b6a5
francesco-guttuso <francescoguttuso@gmail.com>
2026-07-19 23:36:31 +0200

    feat: update styling and structure of Items component for improved layout and user experience

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,10 +1,13 @@
 import "./Items.css";
 export const Items = (props) => (
   <>
-    <ul>
+    <ul className="book-list">
       {props.items.map((item) => (
-        <li key={item.title}>
-          {item.title}{" "}
+        <li key={item.title} className="book-item">
+          <div className="book-info">
+            <span className="book-title">{item.title}</span>
+            <span className="book-author">di {item.author}</span>
+          </div>
           <button
             className="delete"
             onClick={() =>
